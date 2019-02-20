221931e5c019d1d6d78dbd768846ced819f4ebd6
BuildTools <git@kelm.net>
2019-02-20 21:50:00 +0000

    route53 call without credentials configured + delete resource record added

--- bin/setup_dns_route53.py
+++ bin/setup_dns_route53.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
 import boto3
@@ -15,14 +15,20 @@ def showusage():
     return
 
 # Check parameters
-if len(sys.argv) > 2 :
-  Hostname = sys.argv[1]
-  IpAddress = sys.argv[2]
+if len(sys.argv) > 1 :
+    Hostname = sys.argv[1]
 else:
     showusage()
     raise Exception("Missing command line parameters.")
 
+if len(sys.argv) > 2 :
+    IpAddress = sys.argv[2]
+else:
+    IpAddress = None
+
 # Load configuration
+ConfigLoaded = False
+HostedZoneId = None
 try: 
     config = toml.load(open(config_file_path))
     if not "credentials" in config: 
@@ -52,39 +58,99 @@ try:
             raise Exception("No Hosted Zone ID configured for \"" + Hostname + "\".")
         HostedZone = HostedZone[i+1:]
 
-# bla.blubber.de
+    ConfigLoaded = True
 except FileNotFoundError:
     print("Configuration file " + config_file_path + " not found.")
-    print("Exiting.")
-    exit(1)
 
+# Try to load configuration from environment    
+if not ConfigLoaded and os.getenv('AWS_ACCESS_KEY_ID') :
+    print("Loading aws credentials from environment.")
+    aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+    aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+    ConfigLoaded = True
 
-# Load aws credentials
-route53 = boto3.client(
-    "route53",
-    aws_access_key_id = aws_secret_key_id,
-    aws_secret_access_key = aws_secret_access_key)
+# Pass aws credentials if given.
+if ConfigLoaded :
+    route53 = boto3.client(
+        "route53",
+        aws_access_key_id = aws_access_key_id,
+        aws_secret_access_key = aws_secret_access_key)
+else:
+    route53 = boto3.client(
+        "route53")
 
-response = route53.change_resource_record_sets(
-    HostedZoneId=HostedZoneId,
-    ChangeBatch={
-     'Comment': 'string',
-        'Changes': [
-            {
-                'Action': 'UPSERT',
-                'ResourceRecordSet': {
-                    'Name': Hostname,
-                    'Type': 'A',
-                    'TTL': 60,
-                    'ResourceRecords': [
-                        {
-                            'Value': IpAddress                        },
+# Discover hostedzone id if not known.
+if HostedZoneId is None :
+    HostedZone = Hostname
+    HostedZoneId = ""
+    while not HostedZoneId:
+        response = route53.list_hosted_zones_by_name(
+            DNSName=HostedZone)
+        if len(response['HostedZones']) > 0 :
+            HostedZoneId = response['HostedZones'][0]['Id']
+            break;
+        
+        i = HostedZone.find(".") 
+        if i < 0 :
+            raise Exception("Hosted Zone ID not configured and not listed in AWS account for \"" + Hostname + "\".")
+        HostedZone = HostedZone[i+1:]
+
+        
+# Update Hostename
+
+if not IpAddress is None:
+    # This is a UPSERT operation.
+    response = route53.change_resource_record_sets(
+        HostedZoneId=HostedZoneId,
+        ChangeBatch={
+            "Comment": "string",
+            "Changes": [
+                {
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": Hostname,
+                        "Type": "A",
+                        "TTL": 60,
+                        "ResourceRecords": [
+                            {
+                                "Value": IpAddress                        },
+                        ],
+                    }
+                },
+            ]
+        }
+    )
+    print("Setting \"" + Hostname + "\" to \"" + IpAddress + "\" and TTL = 60.")
+else:
+    # This is a DELETE operation.
+    # Discover existing values for Hostname
+    response = route53.list_resource_record_sets(
+        HostedZoneId=HostedZoneId,
+        StartRecordName=Hostname,
+        StartRecordType='A',
+        MaxItems='1')
+    
+    if len(response['ResourceRecordSets']) > 0 and response['ResourceRecordSets'][0]['Name']==Hostname+'.' :
+        CBatch={
+            "Changes": [
+                {
+                    "Action": "DELETE",
+                    "ResourceRecordSet": {
+                        "Name": Hostname,
+                        "Type": "A",
+                        "TTL" : response['ResourceRecordSets'][0]['TTL'],
+                    "ResourceRecords": [
+                        {  "Value": response['ResourceRecordSets'][0]['ResourceRecords'][0]['Value']                  },
                     ],
+                    }
                 }
-            },
-        ]
-    }
-)
-
-# Check response
-print("Setting \"" + Hostname + "\" to \"" + IpAddress + "\" and TTL = 60.")
+            ]
+        }
+        response = route53.change_resource_record_sets(
+            HostedZoneId=HostedZoneId,
+            ChangeBatch=CBatch
+        )
+        print("Deleting \"" + Hostname + "\" .")
+    else:
+        print('Resource record of type A not found for "' + Hostname + '".')
+        
